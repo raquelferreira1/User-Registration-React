2efc3b54f4c5ee33836dbda2fe56a09bc04c37f1
raquelferreira1 <raquel.riosferreira@gmail.com>
2023-05-29 19:21:46 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import People from './assets/people.svg'
+import Arrow from './assets/arrow.svg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Container, Image, ContainerItens, H1, InputLabel, Input, Button, } from './styles';
+const App = () => {
+
+  return (<Container>
+    <Image alt="logo-img" src={People}/>
+    <ContainerItens>
+    <H1> Olá </H1>
+    <InputLabel> Nome </InputLabel>
+    <Input placeholder='Nome'/>
+
+    <InputLabel> Idade </InputLabel>
+    <Input placeholder='Idade'/>
+
+    <Button> Cadastrar <img alt="seta" src={Arrow}/> </Button>
+    </ContainerItens>
+  </Container>
+  )
 }
 
-export default App;
+
+export default App
