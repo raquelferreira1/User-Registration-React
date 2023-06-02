b80d6e81f59183ea048d23bf5abf61ee503e2ce7
raquelferreira1 <raquel.riosferreira@gmail.com>
2023-06-02 19:52:17 -0300

    feat

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import People from './assets/people.svg'
 import Arrow from './assets/arrow.svg'
+import Trash from './assets/trash.svg'
+import { Container, Image, ContainerItens, H1, InputLabel, Input, Button, User } from './styles';
 
-import { Container, Image, ContainerItens, H1, InputLabel, Input, Button, } from './styles';
-const App = () => {
+function App() {
+  const [users, setUsers] = useState([]);
+  const inputName = useRef();
+  const inputAge = useRef();
 
-  return (<Container>
-    <Image alt="logo-img" src={People}/>
-    <ContainerItens>
-    <H1> Olá </H1>
-    <InputLabel> Nome </InputLabel>
-    <Input placeholder='Nome'/>
+  function addNewUser() {
+    setUsers([...users, {
+      id: Math.random(),
+      name: inputName.current.value,
+      age: inputAge.current.value
+    },
+    ])
+  }
 
-    <InputLabel> Idade </InputLabel>
-    <Input placeholder='Idade'/>
+  function deleteUser(userId) {
+    const newUsers = users.filter((user) => user.id !== userId);
+    setUsers(newUsers);
+  }
 
-    <Button> Cadastrar <img alt="seta" src={Arrow}/> </Button>
-    </ContainerItens>
-  </Container>
+  return (
+    <Container>
+      <Image alt="logo-img" src={People} />
+      <ContainerItens>
+        <H1> Olá </H1>
+        <InputLabel> Nome </InputLabel>
+        <Input ref={inputName} placeholder='Nome' />
+
+        <InputLabel> Idade </InputLabel>
+        <Input ref={inputAge} placeholder='Idade' />
+
+        <Button onClick={addNewUser}>
+          Cadastrar <img alt="seta" src={Arrow} />
+        </Button>
+
+        <ul>
+          {users.map((user) => (
+            <User key={user.id}>
+              <p>{user.name}</p> <p>{user.age}</p>
+
+              <button onClick={() => deleteUser(user.id)}>
+                <img src={Trash} alt="lata-de-lixo" />
+              </button>
+            </User>
+          ))}
+        </ul>
+
+      </ContainerItens>
+    </Container>
   )
 }
 
--- src/styles.js
+++ src/styles.js
@@ -2,83 +2,82 @@ import styled from 'styled-components'
 import Background from './assets/background1.svg'
 
 export const Container = styled.div`
-background: url("${Background}");
-background-size: cover;
-display: flex;
-align-items: center;
-flex-direction: column;
-gap: 40px;
-height: 100vh;
+    background: url("${Background}");
+    background-size: cover;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    gap: 40px;
+    height: 100vh;
 `
 
 export const Image = styled.img`
-margin-top: 30px;
+    margin-top: 30px;
 `
 
 
 export const ContainerItens = styled.div`
-background: linear-gradient(157.44deg, rgba(255, 255, 255, 0.6) 0.84%, rgba(255, 255, 255, 0.6) 0.85%, rgba(255, 255, 255, 0.15) 100%);
-border-radius: 61px 61px 0px 0px;
-padding: 50px 36px;
-display: flex;
-flex-direction: column;
-height: 100vh;
-
-;`
+    background: linear-gradient(157.44deg, rgba(255, 255, 255, 0.6) 0.84%, rgba(255, 255, 255, 0.6) 0.85%, rgba(255, 255, 255, 0.15) 100%);
+    border-radius: 61px 61px 0px 0px;
+    padding: 50px 36px;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+`
 
 
 export const H1 = styled.h1`
-font-style: normal;
-font-weight: 700;
-font-size: 34px;
-line-height: 40px;
-text-align: center;
-color: #FFFFFF;
-margin-bottom:80px;`
+    font-style: normal;
+    font-weight: 700;
+    font-size: 34px;
+    line-height: 40px;
+    text-align: center;
+    color: #FFFFFF;
+    margin-bottom:80px;
+`
 
 export const InputLabel = styled.p`
-font-style: normal;
-font-weight: 700;
-font-size: 18px;
-line-height: 22px;
-letter-spacing: -0.408px;
-color: #EEEEEE;
-margin-left: 25px;
-;`
+    font-style: normal;
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 22px;
+    letter-spacing: -0.408px;
+    color: #EEEEEE;
+    margin-left: 25px;
+`
 
 export const Input = styled.input`
-background: rgba(255, 255, 255, 0.25);
-box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-border-radius: 14px;
-width: 342px;
-height: 58px;
-padding-left: 25px;
-margin-bottom: 34px;
-border: none;
-outline: none;
-font-style: normal;
-font-weight: 400;
-font-size: 20px;
-line-height: 28px;
-color: #FFFFFF;
-
-;`
+    background: rgba(255, 255, 255, 0.25);
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 14px;
+    width: 342px;
+    height: 58px;
+    padding-left: 25px;
+    margin-bottom: 34px;
+    border: none;
+    outline: none;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 28px;
+    color: #FFFFFF;
+`
 
 export const Button = styled.button`
-width: 342px;
-height: 74px;
-background: rgba(0, 0, 0, 0.8);
-border-radius: 14px;
-font-style: normal;
-font-weight: 700;
-font-size: 17px;
-line-height: 28px;
-color: #FFFFFF;
-cursor: pointer;
-display: flex;
-align-items: center;
-justify-content: center;
-gap: 20px;
+    width: 342px;
+    height: 74px;
+    background: rgba(0, 0, 0, 0.8);
+    border-radius: 14px;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 17px;
+    line-height: 28px;
+    color: #FFFFFF;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 20px;
 
 &:hover {
     opacity: 0.8;
@@ -86,4 +85,32 @@ gap: 20px;
 
 &:active {
     opacity: 0.5;
-}`;
+}`
+
+export const User = styled.li`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    background: rgba(255, 255, 255, 0.25);
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 14px;
+    width: 342px;
+    height: 58px;
+    border: none;
+    outline: none;
+    margin-top: 20px;
+
+p {
+    font-style: normal;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 28px;
+    color: #FFFFFF;
+}
+
+button {
+    background: none;
+    border: none;
+    cursor: pointer;
+}
+`
